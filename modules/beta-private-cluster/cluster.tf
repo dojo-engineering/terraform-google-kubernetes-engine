1a5c3f22ffce45ddd55591610afaad9695c70716
David Garnett Welsh <88321759+davidgwps@users.noreply.github.com>
2024-01-04 16:23:32 +0000

    fix: [F2FAG-390] forced node pool recreation
    Fixing issue https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/issues/1823 for Payments Platform in dojo-engineering

--- modules/beta-private-cluster/cluster.tf
+++ modules/beta-private-cluster/cluster.tf
@@ -573,11 +573,8 @@ resource "google_container_node_pool" "pools" {
     image_type       = lookup(each.value, "image_type", "COS_CONTAINERD")
     machine_type     = lookup(each.value, "machine_type", "e2-medium")
     min_cpu_platform = lookup(each.value, "min_cpu_platform", "")
-    dynamic "gcfs_config" {
-      for_each = lookup(each.value, "enable_gcfs", false) ? [true] : []
-      content {
-        enabled = gcfs_config.value
-      }
+    gcfs_config {
+      enabled = lookup(each.value, "enable_gcfs", false)
     }
     dynamic "gvnic" {
       for_each = lookup(each.value, "enable_gvnic", false) ? [true] : []
@@ -807,11 +804,8 @@ resource "google_container_node_pool" "windows_pools" {
     image_type       = lookup(each.value, "image_type", "COS_CONTAINERD")
     machine_type     = lookup(each.value, "machine_type", "e2-medium")
     min_cpu_platform = lookup(each.value, "min_cpu_platform", "")
-    dynamic "gcfs_config" {
-      for_each = lookup(each.value, "enable_gcfs", false) ? [true] : []
-      content {
-        enabled = gcfs_config.value
-      }
+    gcfs_config {
+      enabled = lookup(each.value, "enable_gcfs", false)
     }
     dynamic "gvnic" {
       for_each = lookup(each.value, "enable_gvnic", false) ? [true] : []
